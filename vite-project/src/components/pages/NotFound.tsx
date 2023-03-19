6d93c9e8a5fecf1fd0613f95f1405ca1c1fe25d6
YanaPronko <YaniaR@tut.by>
2023-03-19 21:08:41 +0300

    fix: delete link

--- vite-project/src/components/pages/NotFound.tsx
+++ vite-project/src/components/pages/NotFound.tsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class NotFound extends Component {
   render() {
@@ -7,9 +6,6 @@ class NotFound extends Component {
       <div className="notFound__wrap">
         <h1 className="notFound__title">404</h1>
         <h2 className="notFound__title">Not Found</h2>
-        <Link className="notFound__link" to="/">
-          GO HOME
-        </Link>
       </div>
     );
   }
