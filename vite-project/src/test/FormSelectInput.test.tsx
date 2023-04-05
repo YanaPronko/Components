7056ec4070c43fa8bd50af9a693f6ecfdf8c58e8
YanaPronko <YaniaR@tut.by>
2023-04-05 15:17:47 +0300

    add test to valid card

--- vite-project/src/test/FormSelectInput.test.tsx
+++ vite-project/src/test/FormSelectInput.test.tsx
@@ -1,14 +1,6 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import FormPage from '../components/pages/FormPage';
-import FormSelectInput from '../components/inputs/FormSelectInput';
-// import { FieldErrors, FieldValues, useForm, UseFormRegister } from 'react-hook-form';
-// import { IValidCard } from '../components/pages/FormPage';
-// import { Form } from 'react-router-dom';
-// import { mockComponent } from 'react-dom/test-utils';
-
-/* const register: UseFormRegister<IValidCard> = ''
-const errors: FieldErrors<FieldValues>; */
 
 describe('Render Select input', () => {
   it('render', () => {
---
+++ vite-project/src/test/ValidCard.test.tsx
@@ -0,0 +1,21 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ValidCard from '../components/validCard/ValidCard';
+import { IValidInputCard } from '../components/pages/FormPage';
+
+const item: IValidInputCard = {
+  name: 'mockname',
+  birthday: '2020-10-05',
+  gender: 'female',
+  country: 'Belarus',
+  img: 'mockpath',
+  accept: 'true',
+};
+
+describe('Valid card', () => {
+  it('renders valid card component after submitting form', () => {
+    render(<ValidCard item={item} />);
+    expect(screen.getByText(/female/i)).toBeInTheDocument();
+    expect(screen.getByText(/Belarus/i)).toBeInTheDocument();
+  });
+});
